ca83940f65429d15801f97a40b752e35d685f225
Vivek Patel <vivekpatel2991@gmail.comm>
2026-08-14 15:01:31 +0530

    tailwind css practice

--- src/App-49.jsx
+++ src/App-49.jsx
@@ -1,9 +1,16 @@
 function App49() {
     return (
-    <div className="bg-blue-500 text-white text-3xl font-bold p-10">
-      Tailwind is working!
-    </div>
-  );
+        <div>
+            <div className="flex items-center justify-center bg-blue-500 text-white text-3xl font-bold p-10">
+                Tailwind is working!
+            </div>
+            <div className="flex items-center justify-center h-screen bg-blue-100">
+                <h1 className="text-4xl font-bold text-blue-600 underline">
+                    Tailwind is working!
+                </h1>
+            </div>
+        </div>
+    );
 }
 
 export default App49
